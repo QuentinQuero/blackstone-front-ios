359c19537614158e825969cfd8e329a4a100dd90
carl <carl1.marchand@epitech.eu>
2021-01-20 10:12:44 +0100

    project creation

---
+++ Blackstone Fortress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 20/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
