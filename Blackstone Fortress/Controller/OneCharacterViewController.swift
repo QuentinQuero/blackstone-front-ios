d442d23036a80ea4cc997b2b28af74256a3cdee4
carl <carl1.marchand@epitech.eu>
2021-01-25 21:24:11 +0100

    character model

--- Blackstone Fortress/Controller/OneCharacterViewController.swift
+++ Blackstone Fortress/Controller/OneCharacterViewController.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 class OneCharacterViewController: UIViewController {
-    @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var testLabel: UILabel!
     
-    var perso: Character!
+    var character: Character!
+    // http: //localhost:3000/image/character/explorator/UR-025.jpeg -> IMAGE
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        nameLabel.text = perso.name
-        testLabel.text = perso.name
+        nameLabel.text = character.name
+        testLabel.text = "\(character.life)"
     }
     
 
