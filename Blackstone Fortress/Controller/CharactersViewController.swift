3c78be1163ff3400dda9f0f5b87f88808860bcc8
carl <carl1.marchand@epitech.eu>
2021-01-25 10:28:19 +0100

    archi storyboard

---
+++ Blackstone Fortress/Controller/CharactersViewController.swift
@@ -0,0 +1,119 @@
+//
+//  CharactersViewController.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 22/01/2021.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class CharactersViewController: UITableViewController {
+    var characters = [Character]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        getCharacters()
+    }
+    
+    private func getCharacters() {
+        AF.request("http://localhost:3000/character").responseJSON { response in
+            do {
+                let jsonData = response.data!
+                let json = try JSON(data: jsonData)
+                
+                let res = json["data"]
+
+                for (_, value) in res {
+                    if let name = value["name"].string {
+                        
+                        let perso = Character(name: name)
+                        self.characters.append(perso)
+                    }
+                    print(self.characters)
+                }
+            
+            } catch {
+                return
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return  self.characters.count
+//        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath)
+
+        // Configure the cell...
+        let name =  self.characters[indexPath.row].name
+        cell.textLabel?.text = name
+        cell.detailTextLabel?.text = "test"
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Blackstone Fortress/Model/Character.swift
@@ -0,0 +1,53 @@
+//
+//  Character.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 20/01/2021.
+//
+
+import Foundation
+
+struct Character {
+    var name: String
+//    var life: Int
+//    var movement: Int
+//    var defense: String
+//    var defense_exalte: String
+//    var agility: String
+//    var agility_exalte: String
+//    var vitality: String
+//    var vitality_exalte: String
+//    var capacity: Dictionary<String, Any>
+////        [{ attack: String range: [{"1": String"2-3": String"4+": String}]}]
+//    var capacity_exalte: [{ attack: String range: [{"1": String"2-3": String"4+": String}]}]
+//    var specialAttack: String
+//    var specialAttack_exalte: String
+//    var specialRole: [{name: String description: String}]
+//    var specialRole_exalte: [{name: String description: String}]
+//    var exalte: [{possible: Boolean description: String}]
+//    var image: String
+//    var vaisseau_id: String
+//
+    init(name: String) {
+//         , life: Int, movement: Int, defense: String, defense_exalte: String, agility: String, agility_exalte: String, vitality: String, vitality_exalte: String, capacity: ,capacity_exalte: ,specialAttack: String, specialAttack_exalte: String, specialRole: , specialRole_exalte: , exalte: , image: String, vaisseau_id: String) {
+        self.name = name
+//        self.life = life
+//        self.movement = movement
+//        self.defense = defense
+//        self.defense_exalte = defense_exalte
+//        self.agility = agility
+//        self.agility_exalte = agility_exalte
+//        self.vitality = vitality
+//        self.vitality_exalte = vitality_exalte
+//        self.capacity = capacity
+//        self.capacity_exalte = capacity_exalte
+//        self.specialAttack = specialAttack
+//        self.specialAttack_exalte = specialAttack_exalte
+//        self.specialRole = specialRole
+//        self.specialRole_exalte = specialRole_exalte
+//        self.exalte = exalte
+//        self.image = image
+//        self.vaisseau_id = vaisseau_id
+    }
+
+}
