936a06abb24fe701687cd94596e99645290d28e7
carl <carl1.marchand@epitech.eu>
2021-01-26 15:56:41 +0100

    challenges get and storyboard

---
+++ Blackstone Fortress/Controller/Challenge/OneChallengeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OneChallengeViewController.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 26/01/2021.
+//
+
+import UIKit
+
+class OneChallengeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var challenge: Challenge!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.titleLabel.text = challenge.title
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
