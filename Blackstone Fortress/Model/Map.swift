89445cd4ff051a7e0419ee02469cb99e8b45722f
carl <carl1.marchand@epitech.eu>
2021-01-26 14:56:01 +0100

    test stash 2

---
+++ Blackstone Fortress/Model/Map.swift
@@ -0,0 +1,19 @@
+//
+//  Map.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 26/01/2021.
+//
+
+import Foundation
+
+struct Map {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
