ae9808a7ba504c22739f07738d76ca6014812701
carl <carl1.marchand@epitech.eu>
2021-01-30 16:08:02 +0100

    fix character

---
+++ Blackstone Fortress/Model/Ennemy.swift
@@ -0,0 +1,8 @@
+//
+//  Ennemy.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 30/01/2021.
+//
+
+import Foundation
---
+++ Blackstone Fortress/Model/ExplorerService.swift
@@ -0,0 +1,8 @@
+//
+//  ExplorerService.swift
+//  Blackstone Fortress
+//
+//  Created by Carl on 30/01/2021.
+//
+
+import Foundation
